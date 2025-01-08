3e5aab16ddeec08edc83eb1e1ed6fd70d8c87a35
unknown <hovietanh147@gmail.com>
2025-01-08 07:39:12 +0700

    Add project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-
+import TetWebsite from '../assets/pc.png';
+import ChristmasWebsite from '../assets/merry.png';
 const projects = [
   { 
     title: 'Dong Thap Cemetery',
     description: 'I work as the Front-end leader of this project. This project helps people find the location of the grave.',
     link: 'https://github.com/VNCemetery',
-    tech: ['React', 'TailwindCSS', 'Node.js', 'MongoDB'],
+    tech: ['React', 'TailwindCSS', 'Node.js', 'PostgreSQL'],
     image: 'https://tuoitudong.vn/wp-content/uploads/2016/10/Ngh%C4%A9a-Trang-Li%E1%BB%87t-S%E1%BB%B9-%C4%90%E1%BB%93ng-Th%C3%A1p-1-1.jpg', 
     cardColor: 'from-[#FEA837] to-[#DE741C]',
     titleColor: 'text-white',
@@ -17,7 +18,7 @@ const projects = [
     title: 'Analyze High School Scores',
     description: 'This website is used to analyze high school exam scores 2018 and 2019.',
     link: 'https://github.com/Analyze-high-school-scores',
-    tech: ['React', 'Flask', 'Ant Design', 'DaisyUI'],
+    tech: ['React', 'Flask', 'TailwindCSS'],
     image: 'https://media.vneconomy.vn/w800/images/upload/2023/06/09/khuyen-hoc-soc-trang-7.jpg',
     role: 'Full Stack Developer',
     cardColor: 'from-[#DE741C] to-[#B85B56]',
@@ -26,11 +27,11 @@ const projects = [
     techBadgeColor: 'bg-white/20 text-white'
   },
   { 
-    title: 'Student Expense Manager',
-    description: 'This website helps students manage their expenses easily.',
-    link: 'https://github.com/Deadline-Student-Manage/Web',
-    tech: ['React', 'Express', 'NoSQL'],
-    image: 'https://summer.harvard.edu/wp-content/uploads/sites/7/2022/10/Time-management-tips.jpg',
+    title: 'HCMUTE - DSC Club Website',
+    description: 'Official website of HCMUTE - DSC Club, featuring news updates, club introduction, events, projects and member profiles.',
+    link: 'https://github.com/AndyAnh174/HCMUTE-DSC',
+    tech: ['React', 'Flask SwaggerUI', 'NoSQL', 'TailwindCSS'],
+    image: 'https://scontent.fsgn2-3.fna.fbcdn.net/v/t39.30808-6/471555547_625905189961709_1280924454691844356_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeEsTtM5AbAtGWIhdL1j72zxnQidnNltTgydCJ2c2W1ODHrtTFzaSMCZsBtraxR9LTdYAdTF8gbacKppsR6ruiMc&_nc_ohc=CICXpMeG2gIQ7kNvgEfXaxP&_nc_oc=AdhaWfY-8nFtNi3sClFLqYYY-yu4RQa3MTNXPv8nIuJYAfnJ68HYClk219frzIWTU1E&_nc_zt=23&_nc_ht=scontent.fsgn2-3.fna&_nc_gid=AVUfKT6mtM6sDoWWnOWIZgX&oh=00_AYBPRPp0C7_jtGQMfsqVXxWumQQMur-StAoIglbw67mKNg&oe=678381D7',
     role: 'Full Stack Developer',
     cardColor: 'from-[#84495F] to-[#593E67]',
     titleColor: 'text-white',
@@ -38,11 +39,23 @@ const projects = [
     techBadgeColor: 'bg-white/20 text-white'
   },
   { 
-    title: 'FIT Kitchen',
-    description: 'This website helps students order food to support the "Spring Volunteer" campaign.',
-    link: 'https://github.com/toantc1024/itute',
-    tech: ['Html/Css/Js'],
-    image: 'https://scontent.fsgn2-6.fna.fbcdn.net/v/t39.30808-6/409819661_355301903815800_1728684963116372009_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeGc38_-f29fYEc_5PpFogsd6uSB7pUJ7LXq5IHulQnstWKzeQ2QLdyxRZvmcQNoKbL8VlURXdPCZ65BoEoNtmZw&_nc_ohc=NAzCqO4yilwQ7kNvgHCZkye&_nc_zt=23&_nc_ht=scontent.fsgn2-6.fna&_nc_gid=AM4MpDn2QfdWR7vJe7B6iFQ&oh=00_AYAzhwFXgZg8azO4J1XA7QlqEp3nr7fTIUqJdgs58-fylQ&oe=67609988',
+    title: 'HCMUTE-DSC Tet Website',
+    description: 'A festive website for creating and sharing Lunar New Year wishes, drawing lucky fortunes, and spreading Tet greetings within the HCMUTE community.',
+    link: 'https://github.com/AndyAnh174/project-tet-fe',
+    tech: ['React', 'TailwindCSS', "Flask", "NoSQL"],
+    image: TetWebsite,
+    role: 'Full Stack Developer',
+    cardColor: 'from-[#593E67] to-[#84495F]',
+    titleColor: 'text-white',
+    descColor: 'text-white/90',
+    techBadgeColor: 'bg-white/20 text-white'
+  },
+  {
+    title: 'HCMUTE-DSC Christmas Website',
+    description: 'A Christmas website that allows people to create greeting cards and decorate profile pictures with special Christmas motifs.',
+    link: 'https://github.com/AndyAnh174/Merry-Christmas',
+    tech: ['React', 'TailwindCSS'],
+    image: ChristmasWebsite,
     role: 'Frontend Developer',
     cardColor: 'from-[#593E67] to-[#84495F]',
     titleColor: 'text-white',
